fd2802eaffb9c94a3dcc143bacbb1b856ff5a59e
kimstars <buikika9@gmail.com>
2023-01-03 14:04:53 +0700

    QLDIEM-3 review thiết kế cho sang

--- TTNhom-QLDiem/GUI/Login.Designer.cs
+++ TTNhom-QLDiem/GUI/Login.Designer.cs
@@ -30,6 +30,7 @@ namespace TTNhom_QLDiem.GUI
         private void InitializeComponent()
         {
             this.grpCtrl1 = new DevExpress.XtraEditors.GroupControl();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
             this.btnOK = new DevExpress.XtraEditors.SimpleButton();
             this.checkEditNhoMatKhau = new DevExpress.XtraEditors.CheckEdit();
@@ -38,14 +39,13 @@ namespace TTNhom_QLDiem.GUI
             this.lbPass = new DevExpress.XtraEditors.LabelControl();
             this.txtUser = new DevExpress.XtraEditors.TextEdit();
             this.lbUser = new DevExpress.XtraEditors.LabelControl();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.grpCtrl1)).BeginInit();
             this.grpCtrl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkEditNhoMatKhau.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkEditHienMatKhau.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // grpCtrl1
@@ -72,19 +72,30 @@ namespace TTNhom_QLDiem.GUI
             this.grpCtrl1.LookAndFeel.UseDefaultLookAndFeel = false;
             this.grpCtrl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.grpCtrl1.Name = "grpCtrl1";
-            this.grpCtrl1.Size = new System.Drawing.Size(553, 291);
+            this.grpCtrl1.Size = new System.Drawing.Size(553, 274);
             this.grpCtrl1.TabIndex = 2;
             this.grpCtrl1.Text = "Thông tin đăng nhập";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Azure;
+            this.pictureBox1.Image = global::TTNhom_QLDiem.Properties.Resources.japan_lofi;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // btnCancel
             // 
             this.btnCancel.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancel.Appearance.Options.UseFont = true;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(304, 225);
+            this.btnCancel.Location = new System.Drawing.Point(304, 212);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(114, 30);
+            this.btnCancel.Size = new System.Drawing.Size(114, 28);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Hủy bỏ";
             // 
@@ -93,16 +104,16 @@ namespace TTNhom_QLDiem.GUI
             this.btnOK.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOK.Appearance.Options.UseFont = true;
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(131, 225);
+            this.btnOK.Location = new System.Drawing.Point(131, 212);
             this.btnOK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(114, 30);
+            this.btnOK.Size = new System.Drawing.Size(114, 28);
             this.btnOK.TabIndex = 1;
             this.btnOK.Text = "Đăng nhập";
             // 
             // checkEditNhoMatKhau
             // 
-            this.checkEditNhoMatKhau.Location = new System.Drawing.Point(179, 179);
+            this.checkEditNhoMatKhau.Location = new System.Drawing.Point(179, 168);
             this.checkEditNhoMatKhau.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkEditNhoMatKhau.Name = "checkEditNhoMatKhau";
             this.checkEditNhoMatKhau.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -113,7 +124,7 @@ namespace TTNhom_QLDiem.GUI
             // 
             // checkEditHienMatKhau
             // 
-            this.checkEditHienMatKhau.Location = new System.Drawing.Point(390, 148);
+            this.checkEditHienMatKhau.Location = new System.Drawing.Point(390, 139);
             this.checkEditHienMatKhau.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkEditHienMatKhau.Name = "checkEditHienMatKhau";
             this.checkEditHienMatKhau.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -124,7 +135,7 @@ namespace TTNhom_QLDiem.GUI
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(177, 143);
+            this.txtPass.Location = new System.Drawing.Point(177, 135);
             this.txtPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPass.Name = "txtPass";
             this.txtPass.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -137,7 +148,7 @@ namespace TTNhom_QLDiem.GUI
             // 
             this.lbPass.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbPass.Appearance.Options.UseFont = true;
-            this.lbPass.Location = new System.Drawing.Point(179, 116);
+            this.lbPass.Location = new System.Drawing.Point(179, 109);
             this.lbPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lbPass.Name = "lbPass";
             this.lbPass.Size = new System.Drawing.Size(81, 23);
@@ -146,7 +157,7 @@ namespace TTNhom_QLDiem.GUI
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(179, 71);
+            this.txtUser.Location = new System.Drawing.Point(179, 67);
             this.txtUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtUser.Name = "txtUser";
             this.txtUser.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -158,40 +169,29 @@ namespace TTNhom_QLDiem.GUI
             // 
             this.lbUser.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbUser.Appearance.Options.UseFont = true;
-            this.lbUser.Location = new System.Drawing.Point(179, 44);
+            this.lbUser.Location = new System.Drawing.Point(179, 41);
             this.lbUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lbUser.Name = "lbUser";
             this.lbUser.Size = new System.Drawing.Size(122, 23);
             this.lbUser.TabIndex = 0;
             this.lbUser.Text = "Tên đăng nhập";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Azure;
-            this.pictureBox1.Image = global::TTNhom_QLDiem.Properties.Resources.japan_lofi;
-            this.pictureBox1.Location = new System.Drawing.Point(6, 44);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(157, 157);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 291);
+            this.ClientSize = new System.Drawing.Size(553, 274);
             this.Controls.Add(this.grpCtrl1);
             this.Name = "Login";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.grpCtrl1)).EndInit();
             this.grpCtrl1.ResumeLayout(false);
             this.grpCtrl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkEditNhoMatKhau.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkEditHienMatKhau.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
